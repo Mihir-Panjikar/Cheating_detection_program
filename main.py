552c5796cb2f4b5cbe9ac90276e9eabdf2fcb7cd
Mihir Panjikar <mihirpanjikar@gmail.com>
2024-02-03 23:11:09 +0530

    Added eye detection feature

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 # PHONE_URL = 'https://Phone IP address:PORT/video'
 
 face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_classifier = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 cam = cv2.VideoCapture(os.environ["PHONE_URL"])
 
@@ -17,11 +18,18 @@ while True:
 
     if ret:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        
         faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        cv2.imshow('Camera Feed', frame)
+            
+            roi_gray = gray[y:y+h, x:x+h]
+            roi_color = frame[y:y+h, x:x+h]
+            eyes = eye_classifier.detectMultiScale(roi_gray)
+            
+            for(ex, ey, ew, eh) in eyes:
+                cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (255, 255, 0), 2)
+                cv2.imshow('Camera Feed', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
